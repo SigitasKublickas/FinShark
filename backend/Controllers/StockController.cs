88db4821cdb2bdd329dfb320836d7c907b971898
Sigitas <sig.kublickas99@gmail.com>
2024-02-10 12:38:47 +0200

    add async and await

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using backend.Data;
 using backend.Dtos.Stock;
 using backend.Mappers;
+using backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {   //Apsirašomas norimas Route
@@ -19,19 +21,19 @@ namespace backend.Controllers
             _context = context;
         }
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
             //sutvarkoma masyvo objektu forma pagal ToStockDto() 
-            var stocks = _context.Stocks.ToList()
-            .Select(s=>s.ToStockDto());
+            var stocks = await _context.Stocks.ToListAsync();
+            var stockDto = stocks.Select(s=>s.ToStockDto());
 
-            return Ok(stocks);
+            return Ok(stockDto);
         }
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
             // Ieškomas įrašas pagal Id
-            var stock = _context.Stocks.Find(id);
+            var stock = await _context.Stocks.FindAsync(id);
 
             if(stock == null)return NotFound();
 
@@ -39,14 +41,14 @@ namespace backend.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
             // sukuriamas tinkamos formos stock įrašas
             var stockModel = stockDto.ToStockToCreateDto();
             // pridedama į duomenų bazę
-            _context.Stocks.Add(stockModel);
+            await _context.Stocks.AddAsync(stockModel);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetById) , new {id = stockModel.Id} , stockModel.ToStockDto());
                                 //panaudojamas GetById \ Paduodamas sukurtas  \ paduodama kokiu formatu 
@@ -55,9 +57,9 @@ namespace backend.Controllers
 
         [HttpPut("{id}")]
         
-        public IActionResult Update([FromRoute] int id , [FromBody] UpdateStockRequestDto updateDto)
+        public async Task<IActionResult> Update([FromRoute] int id , [FromBody] UpdateStockRequestDto updateDto)
         {
-            var stockModel = _context.Stocks.FirstOrDefault(x=>x.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x=>x.Id == id);
             // Ieškoma ar egzistuoja
 
             if(stockModel==null)return NotFound();
@@ -69,22 +71,22 @@ namespace backend.Controllers
             stockModel.LastDiv = updateDto.LastDiv;
             stockModel.Industry = updateDto.Industry;
             stockModel.MarketCap = updateDto.MarketCap;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return Ok(stockModel.ToStockDto());
 
         }
         [HttpDelete("{id}")]
 
-        public IActionResult Delete([FromRoute] int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
-             var stockModel = _context.Stocks.FirstOrDefault(x=>x.Id == id);
+             var stockModel = await _context.Stocks.FirstOrDefaultAsync(x=>x.Id == id);
             // Ieškoma ar egzistuoja
             if(stockModel==null)return NotFound();
 
             _context.Stocks.Remove(stockModel);
             
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
